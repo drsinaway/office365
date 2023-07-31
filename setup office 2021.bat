c1cfc86e6a95c92c80455d417d40cb983001db9b
Dr.SINAWAY <118053510+drsinaway@users.noreply.github.com>
2023-07-31 06:54:54 +0300

    Add files via upload

---
+++ setup office 2021.bat
@@ -0,0 +1,7 @@
+@echo off
+cls
+color 2
+title Supported products:&echo __________________________________________________________________________&echo Office 2021 activation... Activation By Dr.SINAWAY&echo website: https://www.drsinaway.com&echo __________________________________________________________________________
+
+
+setup /configure configuration.xml
